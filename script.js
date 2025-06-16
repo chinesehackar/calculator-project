e31fb31f9f9dce69785b7f8503e787b8b8d037ae
chinesehackar <212491917+chinesehackar@users.noreply.github.com>
2025-06-16 11:21:09 +0800

    Switch case for each operator

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 let firstNum;
 let operator
 let secondNum;
+let result;
+
+function operate(num1, operator, num2) {
+    switch(operator) {
+        case "+": {
+            result = add(num1, num2)
+            break;
+        };
+        case "-": {
+            result = subtract(num1, num2)
+            break;
+        }
+        case "*": {
+            result = multiply(num1, num2)
+            break;
+        }
+        case "/": {
+            result = divide(num1, num2)
+            break;
+        }
+    }
+}
 
 function add(a, b) {
     return a + b;
