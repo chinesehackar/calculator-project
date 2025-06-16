40719ba5218c2291b0bd9431a4b3e326dc10d760
chinesehackar <212491917+chinesehackar@users.noreply.github.com>
2025-06-16 21:07:53 +0800

    Handle edge cases.
    The user can input operator sign multiple times and
    press equal without any operators or number without returning NaN and
    clear operation history with all-clear.

--- script.js
+++ script.js
@@ -1,7 +1,57 @@
 let firstNum;
-let operator
+let operator = "";
 let secondNum;
 let result;
+let readyClear = false;
+
+const display = document.querySelector(".display");
+const interface = document.querySelector(".interface");
+interface.addEventListener("click", populateDisplay)
+
+function populateDisplay(e) {
+    if (e.target.matches(".digit")) {
+        if (readyClear) {
+            clear();
+            readyClear = false;
+        }
+        display.textContent += e.target.textContent
+    }
+    if (e.target.matches(".operator")) {
+        if (operator === "") {
+            firstNum = parseFloat(display.textContent);
+            operator = e.target.textContent
+            display.textContent = "";
+        }
+        readyClear = false;
+    }
+    if (e.target.matches(".equals")) {
+        if (!firstNum && !operator) { //no vars at all
+            firstNum = parseFloat(display.textContent);
+            display.textContent = firstNum
+        } else if (firstNum && operator && !display.textContent) { //first num and operator present
+            display.textContent = firstNum;
+            operator = "";
+        } else {
+            secondNum = parseFloat(display.textContent);
+            operate(firstNum, operator, secondNum)
+            display.textContent = Math.round(result * 1000000) / 1000000;
+            firstNum = parseFloat(display.textContent);
+            operator = "";
+            readyClear = true;
+        }
+    }
+
+    if (e.target.matches(".all-clear")) {
+        clear();
+    }
+}
+
+function clear() {
+    firstNum = 0;
+    secondNum = 0;
+    operator = "";
+    display.textContent = "";
+}
 
 function operate(num1, operator, num2) {
     switch(operator) {
